209c4da3e97bb251bd20bb6a3a96f298e9000f27
gonzalodr <129683931+gonzalodr@users.noreply.github.com>
2026-01-28 13:09:09 -0600

    fix text documentation

--- kanux-web/src/components/profile/DynamicLanguageList.tsx
+++ kanux-web/src/components/profile/DynamicLanguageList.tsx
@@ -229,7 +229,7 @@ export function DynamicLanguageList({languages,availableLanguages,onChange,onAdd
         </div>
       )}
 
-      {/* Mensaje de cambios pendientes */}
+      {/* pendents*/}
       {pendingChanges && (
         <div className="p-3 bg-yellow-50 border border-yellow-200 rounded-lg">
           <p className="text-sm text-yellow-800">
